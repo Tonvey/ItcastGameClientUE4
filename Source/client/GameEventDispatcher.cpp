726008a7c2ae7d2fb62efa641f570efc58fcbb17
liangboyan <liangboyanzaz@163.com>
2020-01-07 15:14:09 +0800

    dispatcher

--- Source/client/GameEventDispatcher.cpp
+++ Source/client/GameEventDispatcher.cpp
@@ -2,6 +2,7 @@
 
 
 #include "GameEventDispatcher.h"
+using namespace std;
 
 GameEventDispatcher::GameEventDispatcher()
 {
@@ -16,9 +17,50 @@ void GameEventDispatcher::Init()
     auto &event = NetworkController::GetInstance().GetOnNewMessage();
     event.AddRaw(this,&GameEventDispatcher::OnNewGameMessage);
 }
-void GameEventDispatcher::OnNewGameMessage(GameSingleTLV::GameMsgType type,::google::protobuf::Message *msg)
+void GameEventDispatcher::OnNewGameMessage(GameSingleTLV::GameMsgType type,::google::protobuf::Message *_msg)
 {
     UE_LOG(LogTemp, Display, TEXT("GameEventDispatcher::OnNewGameMessage %d "), type );
+    switch(type)
+    {
+    case GameSingleTLV::GAME_MSG_LOGON_SYNCPID:
+        {
+            auto msg = static_cast<pb::SyncPid*>(_msg);
+            int pid = msg->pid();
+            string userName = msg->username();
+            mOnSyncPid.Broadcast(pid);
+            mOnSyncPlayerName.Broadcast(userName);
+            //UE_LOG(LogTemp, Display, TEXT("on pid %d  , on name : %s"), pid,UTF8_TO_TCHAR(userName.c_str()));
+            break;
+        }
+    case GameSingleTLV::GAME_MSG_BROADCAST:
+        {
+            break;
+        }
+    case GameSingleTLV::GAME_MSG_LOGOFF_SYNCPID:
+        {
+            break;
+        }
+    case GameSingleTLV::GAME_MSG_SUR_PLAYER:
+        {
+            break;
+        }
+    case GameSingleTLV::GAME_MSG_SKILL_BROAD:
+        {
+            break;
+        }
+    case GameSingleTLV::GAME_MSG_SKILL_CONTACT_BROAD:
+        {
+            break;
+        }
+    case GameSingleTLV::GAME_MSG_CHANGE_WORLD_RESPONSE:
+        {
+            break;
+        }
+    default:
+        {
+            break;
+        }
+    }
 }
 
 void GameEventDispatcher::Register(GameMsgID_t id,SingleGameMsgDelegate &callback)
@@ -28,7 +70,7 @@ void GameEventDispatcher::Register(GameMsgID_t id,SingleGameMsgDelegate &callbac
     {
         mGameMsgMap.SetNum(idx,true);
     }
-    mGameMsgMap[idx] = callback;
+    mGameMsgMap[idx].Add(callback);
 }
 void GameEventDispatcher::Unregister(GameMsgID_t id)
 {
--- Source/client/GameEventDispatcher.h
+++ Source/client/GameEventDispatcher.h
@@ -7,15 +7,31 @@
 #include "GameMsg.h"
 #include "Singleton.hpp"
 
+#define DECLARE_EVENT_MEMBER_INSTANCE(ev_name) \
+public:\
+    ev_name Get##ev_name()const\
+    {\
+        return m##ev_name;                       \
+    }\
+private:\
+    ev_name m##ev_name;
 /**
  *
  */
 class CLIENT_API GameEventDispatcher : public Singleton<GameEventDispatcher>
 {
 public:
+    DECLARE_EVENT_OneParam(GameEventDispatcher, OnSyncPid,int);
+    DECLARE_EVENT_OneParam(GameEventDispatcher, OnSyncPlayerName,std::string);
+    DECLARE_EVENT_OneParam(GameEventDispatcher, OnSyncPosition,FVector);
+
+    DECLARE_EVENT_MEMBER_INSTANCE(OnSyncPid);
+    DECLARE_EVENT_MEMBER_INSTANCE(OnSyncPlayerName);
+    DECLARE_EVENT_MEMBER_INSTANCE(OnSyncPosition);
+public:
     typedef ::google::protobuf::Message GameMsg_t;
     DECLARE_DELEGATE_OneParam(SingleGameMsgDelegate,GameMsg_t);
-    typedef TArray<SingleGameMsgDelegate> GameMsgMap_t;
+    typedef TArray<TArray<SingleGameMsgDelegate>> GameMsgMap_t;
     typedef GameSingleTLV::GameMsgType GameMsgID_t;
 	GameEventDispatcher();
 	~GameEventDispatcher();
@@ -23,6 +39,7 @@ public:
     void Register(GameMsgID_t id,SingleGameMsgDelegate &callback);
     void Unregister(GameMsgID_t id);
     void Init();
+
 private:
     GameMsgMap_t mGameMsgMap;
 };
