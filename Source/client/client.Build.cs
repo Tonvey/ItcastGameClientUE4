cd8645f2754a08472884f5fe63f75f3baaabdcce
tonvey <liangboyanzaz@163.com>
2020-02-07 09:14:26 +0800

    add plugin slua

--- Source/client/client.Build.cs
+++ Source/client/client.Build.cs
@@ -21,9 +21,17 @@ public class client : ModuleRules
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
         // PublicIncludePathModuleNames.AddRange(new string[] { "Networking" });
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" , "http" });
 
-        PrivateDependencyModuleNames.AddRange(new string[] { });
+		PrivateDependencyModuleNames.AddRange(new string[] { "slua_unreal", "slua_profile", "Slate", "SlateCore", "UMG", "Http" });
+
+        PrivateIncludePathModuleNames.AddRange(new string[] { "slua_unreal" });
+        PublicIncludePathModuleNames.AddRange(new string[] { "slua_unreal","slua_profile" });
+#if UE_4_21_OR_LATER
+        PublicDefinitions.Add("ENABLE_PROFILER");
+#else
+        Definitions.Add("ENABLE_PROFILER");
+#endif
 
         // Uncomment if you are using Slate UI
         // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
@@ -32,7 +40,6 @@ public class client : ModuleRules
         // PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
         // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-        
         LoadLibprotobuf(Target);
     }
 
